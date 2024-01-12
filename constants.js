a12ab132fee4cf8c2991e8da8c50b480e54912b1
Justin Liu <justinliu@ark7.com>
2024-01-12 09:46:20 -0800

    update ticker

--- constants.js
+++ constants.js
@@ -1,7 +1,8 @@
 // constants.js
 // export const tickers = ['U'];
-export const tickers = ['U', 'AAPL', 'MSFT', 'AMZN', 'NVDA', 'GOOGL', 'META', 'TSLA', 'GOOG', 'BRK.B', 'AVGO', 'UNH', 'JPM', 
-  'FB', 'PYPL', 'V', 'HD', 'BABA', 'INTC', 'CMCSA', 'ADBE', 'PFE', 'NFLX', 'TSM', 'CRM', 'KO', 'NKE', 'ABNB', 'XOM', 'PEP',];
+export const tickers = ['U', 'AAPL', 'MSFT', 'AMZN', 'NVDA', 'GOOGL', 'META', 'TSLA', 'GOOG', 
+'BRK.B', 'AVGO', 'UNH', 'JPM', 'PYPL', 'V', 'HD', 'BABA', 'INTC', 'CMCSA', 'ADBE', 'PFE', 
+'NFLX', 'TSM', 'CRM', 'KO', 'NKE', 'ABNB', 'XOM', 'PEP', 'LDOS', 'CPRT', 'GILD', 'LRCX', 'BLDR'];
   
 export const newsCompanyName = ['The Motley Fool', 'MarketWatch', 'Zacks Investment Research', 'Benzinga'];
 export const delayTime = 3000; // ms
